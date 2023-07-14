448372496ab769f48cb255dfb5f711d73e0feb27
alvaro aguirre <alguirre2@gmail.com>
2023-07-13 21:13:38 -0600

    03 reestructura y consultas

--- app/SPVPS.py
+++ app/SPVPS.py
@@ -1,26 +1,20 @@
-from flask import (Flask, render_template, url_for,
-                   redirect)
+from flask import Flask
+
+#importar views
+from views.home_views import home_views
+from views.product_view import product_views
+from views.report_views import report_views
+from views.user_views import user_views
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'My secret key'
 
-@app.route("/")
-def hello_world():
-    return render_template('menu_adm.html')
-
-@app.route("/hola")
-def cerrar():
-    return render_template('cierreSesion.html')
-
-@app.route("/base")
-def Iniciar():
-    return render_template('base.html')
-
-@app.route("/products")
-def productos():
-    return render_template('products.html')
-@app.route("/login")
-def login():
-    return render_template('login.html')
+#registrar views
+app.register_blueprint(home_views)
+app.register_blueprint(product_views)
+app.register_blueprint(report_views)
+app.register_blueprint(user_views)
 
 if __name__ =='__main__':
     app.run(debug =True)  
